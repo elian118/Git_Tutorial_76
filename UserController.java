e64c27f9ecf90551cd73e8f5e891532fef39e26b
elian118 <elian118@naver.com>
2021-03-17 10:19:47 +0900

    Add UserController.java [Insert]

--- UserController.java
+++ UserController.java
@@ -3,8 +3,16 @@ public class UserController {
 	// 로그인 페이지로 이동
 	@RequestMapping("/user/login.do")
 	public String login() {
-		logger.info("경로 : login") :
+		logger.info("login : login") :
 
 		return ""user/login";
 	}
+
+	@RequestMapping("/user/login.do")
+	public String login() {
+		logger.info("login :  insert") :
+
+		return ""user/insert";
+	}
+
 }
